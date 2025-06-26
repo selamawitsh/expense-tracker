d42b760b7ab0b5a43f571fe88962de7264881afc
selamawitsh <162134523+selamawitsh@users.noreply.github.com>
2025-06-26 15:26:25 +0300

    connecting database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
 
 dotenv.config();
 
@@ -8,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
+
+connectDB()
 
 app.get('/', (req, res) => {
   res.send('Welcome to the server!');   
